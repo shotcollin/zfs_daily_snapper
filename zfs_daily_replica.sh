748039a1249a633789be08970d0b304178b71be5
Lars Schotte <gustik@gustik.eu>
2019-06-23 00:23:25 +0200

    replication activated

--- zfs_daily_replica.sh
+++ zfs_daily_replica.sh
@@ -27,6 +27,12 @@ remote_find_latest_zfs_snapshots() {
   ssh "$localbackuphost" zfs get -r -pH -o name,value -t snapshot creation "$localbackuppath" | awk -f find_latest_snapshot.awk | awk '{ print $1 }'
 }
 
+remote_find_zfs_datasets() {
+  localbackuphost="$1"
+  localbackuppath="$2"
+  ssh "$localbackuphost" zfs list -r -pH -o name -t filesystem "$localbackuppath"
+}
+
 local_find_latest_zfs_snapshots() {
   filteredZFSList="$1"
   completeSnapshotList="`zfs get -r -pH -o name,value -t snapshot creation | awk -f find_latest_snapshot.awk | awk '{ print $1 }'`"
@@ -52,6 +58,7 @@ then \
 fi
 
 remote_latest_zfs_snapshot_list="`remote_find_latest_zfs_snapshots "$backuphost" "$backuppath"`"
+remote_zfs_datasets_list="`remote_find_zfs_datasets "$backuphost" "$backuppath"`"
 filtered_zfs_filesystem_list="`filter_zfs_filesystem_list "$zfsfilesystemslist"`"
 local_latest_zfs_snapshot_list="`local_find_latest_zfs_snapshots "$filtered_zfs_filesystem_list" | awk -f sort_zfs_paths.awk`"
 
@@ -63,6 +70,12 @@ do \
     echo "$local_latest_zfs_snapshot_list_instance found on the remote side, continuing"
   else \
     echo "no $local_latest_zfs_snapshot_list_instance found on the remote side, creating"
-    zfs send "$local_latest_zfs_snapshot_list_instance" | ssh "$backuphost"  zfs receive "${backuppath}/${local_latest_zfs_snapshot_list_instance_fs}"
+    if echo $remote_zfs_datasets_list | grep -e "^${backuppath}/${local_latest_zfs_snapshot_list_instance_fs}$" >/dev/null
+    then \
+      true
+    else \
+      ssh "$backuphost" zfs create -p "${backuppath}/${local_latest_zfs_snapshot_list_instance_fs}"
+    fi
+    zfs send "$local_latest_zfs_snapshot_list_instance" | ssh "$backuphost" zfs receive "${backuppath}/${local_latest_zfs_snapshot_list_instance_fs}"
   fi
 done

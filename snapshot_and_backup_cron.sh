34c14ad9ba90303964bea2725dc29b7e460971cd
Lars Schotte <gustik@gustik.eu>
2019-06-23 13:23:00 +0200

    fixed backofftime to the cron script

--- snapshot_and_backup_cron.sh
+++ snapshot_and_backup_cron.sh
@@ -17,8 +17,9 @@ then \
   if [ -z "$nowaitforamoment" ]
   then \
     randomnumber=`openssl rand -hex 12 | tr -d '[a-f]'`
-    [ -z "$randomnumber" ] && randomnumber="1200" || [ "$randomnumber" -lt 600 ] && randomnumber=`expr $randomnumber + 600`
+    [ -z "$randomnumber" ] && randomnumber="1200"
     waitseconds=`expr $randomnumber \% 3600`
+    [ "$waitseconds" -lt 600 ] && waitseconds=`expr $waitseconds + 600`
     sleep $waitseconds
   fi
   ./zfs_daily_snapper.sh && ./zfs_daily_replica.sh --host "$backuphost" --path "$backuppath" && ./zfs_snap_gc.sh

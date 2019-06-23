d7d6682edc5c1422c37b90b9b4a2955f10f25ceb
Lars Schotte <gustik@gustik.eu>
2019-06-23 13:05:24 +0200

    added cron script host and path relay

--- snapshot_and_backup_cron.sh
+++ snapshot_and_backup_cron.sh
@@ -2,7 +2,16 @@
 
 scriptdirectory=`dirname $0`
 
+while [ "$#" -gt 0 ]
+do \
+  case "$1" in
+    "--host")   shift && [ "$#" -gt 0 ] && backuphost="$1";;
+    "--path")   shift && [ "$#" -gt 0 ] && backuppath="$1";;
+  esac
+  shift
+done
+
 if cd `realpath $scriptdirectory`
 then \
-  ./zfs_daily_snapper.sh && ./zfs_daily_replica.sh && ./zfs_snap_gc.sh
+  ./zfs_daily_snapper.sh && ./zfs_daily_replica.sh --host "$backuphost" --path "$backuppath" && ./zfs_snap_gc.sh
 fi

65638ff599de39f4a06336ade0fecb1cf6c1dc39
Lars Schotte <gustik@gustik.eu>
2019-06-23 13:21:34 +0200

    added some backofftime to the cron script

--- snapshot_and_backup_cron.sh
+++ snapshot_and_backup_cron.sh
@@ -7,11 +7,19 @@ do \
   case "$1" in
     "--host")   shift && [ "$#" -gt 0 ] && backuphost="$1";;
     "--path")   shift && [ "$#" -gt 0 ] && backuppath="$1";;
+    "--nowait") nowaitforamoment="1";;
   esac
   shift
 done
 
 if cd `realpath $scriptdirectory`
 then \
+  if [ -z "$nowaitforamoment" ]
+  then \
+    randomnumber=`openssl rand -hex 12 | tr -d '[a-f]'`
+    [ -z "$randomnumber" ] && randomnumber="1200" || [ "$randomnumber" -lt 600 ] && randomnumber=`expr $randomnumber + 600`
+    waitseconds=`expr $randomnumber \% 3600`
+    sleep $waitseconds
+  fi
   ./zfs_daily_snapper.sh && ./zfs_daily_replica.sh --host "$backuphost" --path "$backuppath" && ./zfs_snap_gc.sh
 fi
